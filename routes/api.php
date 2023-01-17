2f7970c32c8db5b0104b78c04709c58c588f5b0c
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-01-17 15:03:16 +0100

    post, delete, and patch routes

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,27 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
+// get all users
 Route::get('/users', function() {
     return DB::table('users')->get();
+});
+// post user
+Route::post('/users', function() {
+    $id = DB::table('users')->insertGetId([
+    'username' => request('username'),
+    'password' => bcrypt(request('password'))
+    ]);
+    return $id;
+});
+// delete user
+Route::delete('/users/{id}', function ($id) {
+    $todo = DB::table('users')->where('id', $id)->delete();
+    return "deleted";
+});
+// patch password
+Route::patch('/users/{id}', function ($id) {
+    DB::table('users')->where('id', $id)->update([
+    'password' => bcrypt(request('password'))
+    ]);
+    return "password updated";
 });
