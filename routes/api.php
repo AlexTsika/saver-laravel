d9c134251cb3bb14ee2c6e9af3a4e750d4a45fe6
Remco Duckers <rempel.duckers@telenet.be>
2023-01-24 09:33:39 +0100

    added query

--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 // get all users
-Route::get('/users', function() {
+Route::get('/users', function () {
     return DB::table('users')->get();
+    // return DB::select('select * from users');
 });
 // post user
 Route::post('/users', function() {
