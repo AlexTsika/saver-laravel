9eee3d9a630e94c415bc3a75d50e05f5afde3f03
Alexandros Tsikakis <Alexandros.Tsikakis@ymail.com>
2023-02-03 11:46:16 +0100

    yaml file

--- routes/api.php
+++ routes/api.php
@@ -18,76 +18,20 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-// // get all users
-// Route::get('/users', function() {
-//     return DB::table('users')->get();
-// });
-
 // secure route for all users -  token: 2|TwlhWYjUZ7MDX3ksz3xXyhdIqo4OrWVHOOqsPa1z
 Route::middleware(['auth:sanctum'])->get('/users', function() {
     return DB::table('users')->get();
 });
-
-// // route for user by id
-// Route::get('/users/id/{id}', function($id) {
-//     return DB::table('users')->where('id', $id)->first();
-// });
 
 // secure route for user by id
 Route::middleware(['auth:sanctum'])->get('/users/id/{id}', function($id) {
     return DB::table('users')->where('id', $id)->first();
 });
 
-// // route for user by name
-// Route::get('/users/name/{name}', function($name) {
-//     return DB::table('users')->where('name', $name)->first();
-// });
-
 // secure route for user by name
 Route::middleware(['auth:sanctum'])->get('/users/name/{name}', function($name) {
     return DB::table('users')->where('name', $name)->first();
 });
-
-
-// // post user
-// Route::post('/users', function() {
-//     $id = DB::table('users')->insertGetId([
-//     'name' => request('name'),
-//     'password' => bcrypt(request('password'))
-//     ]);
-//     return response()->json([
-//         'message' => 'User created'
-//     ], 201);
-// });
-
-// // post user advanced
-//     Route::post('/users', function (Request $request) {
-//     $name = $request->input('name');
-//     $email = $request->input('email');
-//     $password = $request->input('password');
-
-//     if (DB::table('users')->where('email', $email)->exists()) {
-//         return response()->json([
-//             'message' => 'email already used'
-//         ], 409);
-//     }
-
-//     if (DB::table('users')->where('name', $name)->exists()) {
-//         return response()->json([
-//             'message' => 'username already used'
-//         ], 409);
-//     }
-
-//     DB::table('users')->insert([
-//         'name' => $name,
-//         'password' => Hash::make($password),
-//         'email' => $email
-//     ]);
-
-//     return response()->json([
-//         'message' => 'User created'
-//     ],201);
-// });
 
 // secure post user advanced
 Route::middleware(['auth:sanctum'])->post('/users', function (Request $request) {
@@ -118,14 +62,6 @@ Route::middleware(['auth:sanctum'])->post('/users', function (Request $request) 
     ],201);
 });
 
-// // delete user
-// Route::delete('/users/id/{id}', function ($id) {
-//     DB::table('users')->where('id', $id)->delete();
-//     return response()->json([
-//         'message' => 'User deleted'
-//     ]);
-// });
-
 // secure route for delete user
 Route::middleware(['auth:sanctum'])->delete('/users/id/{id}', function ($id) {
     DB::table('users')->where('id', $id)->delete();
@@ -134,14 +70,6 @@ Route::middleware(['auth:sanctum'])->delete('/users/id/{id}', function ($id) {
     ]);
 });
 
-// // patch password
-// Route::patch('/users/id/{id}', function ($id) {
-//     DB::table('users')->where('id', $id)->update([
-//     'password' => bcrypt(request('password'))
-//     ]);
-//     return "password updated";
-// });
-
 // secure route patch password
 Route::middleware(['auth:sanctum'])->patch('/users/id/{id}', function ($id) {
     DB::table('users')->where('id', $id)->update([
@@ -149,6 +77,7 @@ Route::middleware(['auth:sanctum'])->patch('/users/id/{id}', function ($id) {
     ]);
     return "password updated";
 });
+
 // create tokens with post (needs at least 1 user)
 Route::post('/tokens/create', function (Request $request) {
     $user = App\Models\User::find($request->user_id);
